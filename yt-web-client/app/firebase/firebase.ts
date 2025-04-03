04c351d64fbd2d6d65ed279c7b44af1030edfd66
James Hao <james.s.hao15@gmail.com>
2025-04-03 03:11:47 -0700

    simple ui

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpNQpZJGGLZctMnU-4IhO-FXZD92qOL2Y",
+  authDomain: "yt-clone-bcb32.firebaseapp.com",
+  projectId: "yt-clone-bcb32",
+  appId: "1:503300825649:web:97f1da5f07086b0b5beb10",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+export function signOut() {
+    return auth.signOut();
+}
+
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
